59348af956d4775a9f9f6e79d7224333123991ef
Hrvoje <hbrkljacic@gmail.com>
2022-09-11 19:26:18 -0400

    BeanAnnotation

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -6,6 +6,7 @@ import com.cydeo.bean_annotation.config.RandomConfig;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
 import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
 import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -15,11 +16,14 @@ public class ComputerTest {
 
     public static void main(String[] args) {
 
-
+        //Creating container by using Application Context
         ApplicationContext container = new AnnotationConfigApplicationContext(ComputerConfig.class, RandomConfig.class);
 
-        Monitor theMonitor = container.getBean(Monitor.class);
-        //SonyMonitor theMonitor = container.getBean(SonyMonitor.class); //how to get a specific monitor from monitor container???
+        //Creating container by using BeanFactory
+        BeanFactory context = new AnnotationConfigApplicationContext();
+
+        //Monitor theMonitor = container.getBean(Monitor.class);
+        SonyMonitor theMonitor = container.getBean(SonyMonitor.class); //how to get a specific monitor from monitor container???
         Case theCase = container.getBean(Case.class);
         Motherboard theMotherboard = container.getBean(Motherboard.class);
 
