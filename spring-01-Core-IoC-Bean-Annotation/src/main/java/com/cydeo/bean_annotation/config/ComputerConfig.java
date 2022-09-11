e5c49c31d225fa1bc8adf2de4ba2860878acba90
Hrvoje <hbrkljacic@gmail.com>
2022-09-11 15:27:48 -0400

    BeanAnnotation

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
@@ -1,7 +1,11 @@
 package com.cydeo.bean_annotation.config;
 
+import com.cydeo.bean_annotation.casefactory.Case;
+import com.cydeo.bean_annotation.casefactory.DellCase;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
 import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
+import com.cydeo.bean_annotation.motherboardfactory.AsusMotherboard;
+import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,4 +16,16 @@ public class ComputerConfig {
     public Monitor monitorSony(){
         return new SonyMonitor("25 inch Beast","Acer",25); //Monitor abc = new SonyMonitor(); - Polymorphism
     }
+
+    @Bean
+    public Case caseDell(){
+        return new DellCase("220B","Dell","240");
+    }
+
+    @Bean
+    public Motherboard motherboardAsus(){
+        return new AsusMotherboard("BJ-200","Asus",4,6,"v2.44");
+    }
+
+
 }
