f73a47fc9b1fb0f431234456dfddaa2d13974a05
Hrvoje <hbrkljacic@gmail.com>
2022-09-12 15:57:43 -0400

    AutoWiring

---
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -0,0 +1,13 @@
+package com.cydeo.streotype_annotation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.cydeo.streotype_annotation")
+public class PcConfig {
+
+
+
+
+}
