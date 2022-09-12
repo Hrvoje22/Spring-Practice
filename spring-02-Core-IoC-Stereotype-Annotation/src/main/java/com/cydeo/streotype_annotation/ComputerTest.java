bf296d8559b0a8762fef7244ef601c264e1de6cc
Hrvoje <hbrkljacic@gmail.com>
2022-09-12 14:26:01 -0400

    ComponentAnnotation

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
@@ -1,6 +1,7 @@
 package com.cydeo.streotype_annotation;
 
 import com.cydeo.streotype_annotation.config.PcConfig;
+import com.cydeo.streotype_annotation.monitorfactory.Monitor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,6 +11,8 @@ public class ComputerTest {
 
         ApplicationContext container = new AnnotationConfigApplicationContext(PcConfig.class);
 
+        Monitor theMonitor = container.getBean(Monitor.class);
+        System.out.println(theMonitor.getSize());
 
 
 
--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -6,4 +6,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "com.cydeo.streotype_annotation") //to scan the package (what we want) in search for @Component
 public class PcConfig {
+
+
+
+
 }
