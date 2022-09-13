ed1c519386674aff1847bf38d572a465212cefd1
Hrvoje <hbrkljacic@gmail.com>
2022-09-13 14:48:21 -0400

    App

--- spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -5,13 +5,22 @@ import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.proxy.EmailCommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import com.cydeo.repository.DBCommentRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommentService {
 
     private CommentRepository commentRepository;
     private CommentNotificationProxy commentNotificationProxy;
     //to avoid tightly coupled we used interface - to be loosely
 
+    //we can autowire one by one but constructor option is better
+
+    //@AutoWired comes automatically
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
 
     public void publishComment(Comment comment){
         //save in DB
