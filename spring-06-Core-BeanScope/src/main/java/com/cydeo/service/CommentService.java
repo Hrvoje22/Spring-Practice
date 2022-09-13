422978414141154002cf9350342d84649404ff2c
Hrvoje <hbrkljacic@gmail.com>
2022-09-13 15:39:12 -0400

    AppBeanScope

---
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,37 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentRepository commentRepository; //recommended to put final so it reminds us to create constructor if we forget
+    private final CommentNotificationProxy commentNotificationProxy;
+    //to avoid tightly coupled we used interface - to be loosely
+
+    //we can autowire one by one but constructor option is better
+
+    //@AutoWired comes automatically
+    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        //save in DB
+        commentRepository.storeComment(comment);
+
+        //send email
+        commentNotificationProxy.sendComment(comment);
+
+
+
+
+    }
+
+
+}
