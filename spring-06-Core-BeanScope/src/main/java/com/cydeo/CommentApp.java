072b298d56031176c0ec69b4a10a2716e43341d9
Hrvoje <hbrkljacic@gmail.com>
2022-09-13 15:55:42 -0400

    AppBeanScope

--- spring-06-Core-BeanScope/src/main/java/com/cydeo/CommentApp.java
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/CommentApp.java
@@ -1,4 +1,34 @@
 package com.cydeo;
 
+import com.cydeo.config.CommentConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class CommentApp {
+
+
+    public static void main(String[] args) {
+
+
+        Comment comment  = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(CommentConfig.class);
+
+        CommentService cs1 = context.getBean(CommentService.class);
+        CommentService cs2 = context.getBean(CommentService.class);
+
+        System.out.println(cs1);
+        System.out.println(cs2);
+        System.out.println(cs1==cs2);
+
+        //when we added prototype Spring created one more object in the container
+
+
+
+    }
 }
