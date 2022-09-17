3fe1607dfb2549d0760a9d21e8b0ce7749750c9d
Hrvoje <hbrkljacic@gmail.com>
2022-09-17 17:47:09 +0200

    StereotypeAnnotation

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
@@ -1,6 +1,7 @@
 package com.cydeo.streotype_annotation;
 
 import com.cydeo.streotype_annotation.config.PcConfig;
+import com.cydeo.streotype_annotation.monitorfactory.AcerMonitor;
 import com.cydeo.streotype_annotation.monitorfactory.Monitor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,8 +14,10 @@ public class ComputerTest {
 
         Monitor theMonitor = container.getBean(Monitor.class);
         Monitor theMonitor2 = (Monitor)container.getBean("acerMonitor");
+        Monitor theMonitor3 = container.getBean(AcerMonitor.class); //or ("acerMonitor",Monitor.class)
         System.out.println(theMonitor.getSize());
         System.out.println(theMonitor2.getSize());
+        System.out.println(theMonitor3.getSize());
 
         //with casting we can get the other object that is not Primary
 
