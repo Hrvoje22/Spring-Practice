1befac12de1e0689a9b8046e77057982e018a204
Hrvoje <hbrkljacic@gmail.com>
2022-10-23 15:30:31 -0400

    Repository

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
@@ -1,6 +1,9 @@
 package com.cydeo.repository;
 
-public interface CarRepository {
+import com.cydeo.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car,Long> {
 
 
 
