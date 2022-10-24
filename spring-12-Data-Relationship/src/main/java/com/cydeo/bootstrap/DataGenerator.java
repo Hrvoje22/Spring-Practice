78b2b88b92791be9dca8570ac88d78bf1bb4de1a
Hrvoje <hbrkljacic@gmail.com>
2022-10-24 14:46:51 -0400

    DataRelationship

--- spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -3,6 +3,7 @@ package com.cydeo.bootstrap;
 import com.cydeo.entity.Payment;
 import com.cydeo.entity.PaymentDetail;
 import com.cydeo.enums.Status;
+import com.cydeo.repository.PaymentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,12 @@ import java.time.LocalDate;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
+
+    private final PaymentRepository paymentRepository;
+
+    public DataGenerator(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -24,6 +31,15 @@ public class DataGenerator implements CommandLineRunner {
         PaymentDetail paymentDetail2 = new PaymentDetail(new BigDecimal("90000"),new BigDecimal("5000"),LocalDate.of(2022,4,29));
 
         payment2.setPaymentDetail(paymentDetail2);
+
+        paymentRepository.save(payment1);
+        paymentRepository.save(payment2);
+
+        System.out.println(paymentRepository.findById(2L).get().getPaymentDetail().getCommissionAmount());
+
+        paymentRepository.delete(payment1);
+
+
 
 
     }
