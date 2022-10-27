365429b69d859b6b0d42878d7233aa7cbaf1b715
Hrvoje <hbrkljacic@gmail.com>
2022-10-26 23:06:23 -0400

    CinemaLab

---
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Movie.java
@@ -0,0 +1,27 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Movie extends BaseEntity{
+
+    private String name;
+    @Column(columnDefinition = "DATE")
+    private LocalDate releaseDate;
+    private Integer duration;
+    @Column(columnDefinition = "TEXT")
+    private String summary;
+
+
+
+
+
+}
