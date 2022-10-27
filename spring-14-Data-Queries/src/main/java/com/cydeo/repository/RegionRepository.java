609f983ded9c41ae56fd595be7ec8ff9f4f4d4e7
Hrvoje <hbrkljacic@gmail.com>
2022-10-27 14:36:35 -0400

    Queries

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RegionRepository extends JpaRepository<Region,Long> {
+public interface RegionRepository extends JpaRepository<Region,Integer> {
 
     //Display all regions in Canada
     List<Region> findByCountry(String country);
