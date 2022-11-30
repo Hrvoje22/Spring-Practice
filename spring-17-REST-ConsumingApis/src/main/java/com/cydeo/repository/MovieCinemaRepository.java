c345cca5162b68b0fbb213547008e552fa964d4f
Hrvoje <hbrkljacic@gmail.com>
2022-11-30 17:28:22 -0500

    REST17

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/repository/MovieCinemaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cydeo.repository;
-
-import com.cydeo.entity.MovieCinema;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long> {
-}
