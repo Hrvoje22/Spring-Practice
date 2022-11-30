23529ee09a73c7ea892c0de0f202e2acd0064a0e
Hrvoje <hbrkljacic@gmail.com>
2022-11-30 17:28:28 -0500

    REST17

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -1,14 +1,11 @@
 package com.cydeo;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableFeignClients
 public class Spring17RestConsumingApisApplication {
 
     public static void main(String[] args) {
@@ -20,8 +17,4 @@ public class Spring17RestConsumingApisApplication {
         return new RestTemplate();
     }
 
-    @Bean
-    public ModelMapper mapper() {
-        return new ModelMapper();
-    }
 }
